d08b17a32ef8b74f93d671adb84acd173c2c6dc2
Devins11 <devinfcb42@gmail.com>
2025-12-14 17:09:49 +0700

    refactor: removed runner bean

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,9 +1,16 @@
 package com.app;
 
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -16,5 +23,18 @@ public class Main {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+//        return args -> {
+//            System.out.println("Inspect the beans had by the main class");
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//
+//            for (String bean: beanNames) {
+//                System.out.println(bean);
+//            }
+//        };
+//
+//    }
 
 }
