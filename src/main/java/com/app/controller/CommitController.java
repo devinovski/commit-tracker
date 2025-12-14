f33096a661e7e3bec0565eb0fa463fae40078b00
Devins11 <devinfcb42@gmail.com>
2025-12-14 16:54:49 +0700

    feat(pom): Added DAO methods to count how many records were successfully sent to the Notion API

--- src/main/java/com/app/controller/CommitController.java
+++ src/main/java/com/app/controller/CommitController.java
@@ -3,6 +3,8 @@ package com.app.controller;
 import com.app.Exception.WrongCharacterPositionException;
 import com.app.model.GitlabWebhook;
 import com.app.service.NotionSyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.NestedRuntimeException;
 import org.springframework.dao.DataAccessException;
@@ -14,12 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/gitlab-webhook")
 public class CommitController {
 
     @Autowired
     private NotionSyncService notionSyncService;
+
+    private static Logger statusLogger = LoggerFactory.getLogger("com.status");
 
     @PostMapping("/commit-parser")
     public ResponseEntity<?> handleWebhook(@RequestBody GitlabWebhook gitlabWebhook){
@@ -31,10 +37,14 @@ public class CommitController {
             return new ResponseEntity<>("Record already exists", HttpStatus.CONFLICT);
         }catch (DataAccessException dae){
             return new ResponseEntity<>(dae.getMessage().toString(), HttpStatus.CONFLICT);
-        }catch (WrongCharacterPositionException wcpe){
-            return new ResponseEntity<>(wcpe.getMessage(), HttpStatus.BAD_REQUEST);
+        }catch (WrongCharacterPositionException | NestedRuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }catch (Exception ex){
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }finally {
+            Long succeed = notionSyncService.countSyncedToNotionIsTrue();
+            Long fail = notionSyncService.countSyncedToNotionIsFalse();
+            statusLogger.info("succeed: {}, fail: {}", succeed, Objects.isNull(fail) ? 0 : fail);
         }
     }
 
